78e520714847641db8c7851617203157b4708d5c
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 14:35:44 -0500

    updated docs

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -18,31 +18,3 @@
 
   
 
-  
-/* vsdoc for atropa */
-
-(function (window) {
-    
-
-    window.atropa = {
-        /// <summary></summary>
-        /// <returns type="atropa"/>
-                
-        setAsOptionalArg: function(defaultVal, optionalArg) {
-            /// <summary>Set default values for optional function parameters.</summary>
-            /// <param name="defaultVal" type="Mixed">The default value to set.</param>
-            /// <param name="optionalArg" type="Mixed">A reference to the optional argument.</param>
-            /// <returns type="Mixed">Returns the default value supplied when the optional
-            /// argument is undefined or null. Otherwise, the supplied optional argument
-            /// is returned.</returns>
-        }
-        
-    };
-
-    var $x = window.atropa;
-    $x.__namespace = "true";
-    $x.__typeName = "atropa";
-})(this);
-
-  
-
--- docs/vsdoc/OpenLayersReferences.js
+++ docs/vsdoc/OpenLayersReferences.js
@@ -1,3 +1,2 @@
 
-/// <reference path="atropa-vsdoc.js" />
 
